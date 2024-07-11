dd6f1a6b7035c72bde701d1dc075f2e7b19805bd
Nikhil Siwan <developer.nikk@gmail.com>
2024-07-12 02:49:48 +0530

    HAHAHAH

--- data/index.ts
+++ data/index.ts
@@ -52,7 +52,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently learing about Web3, Blockchain and AI.",
+    title: "Currently learning about Web3, Blockchain and AI.",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
