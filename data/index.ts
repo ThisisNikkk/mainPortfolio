0f8c14449e8776d0bd56257670340b8d24eebf0b
Nikhil Siwan <developer.nikk@gmail.com>
2024-07-12 00:54:18 +0530

    commits

--- data/index.ts
+++ data/index.ts
@@ -83,11 +83,11 @@ export const projects = [
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
+    title: "CityFood - Food Restaurant App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://nikhilsiwan.netlify.app/",
+    img: "/cityFood.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/fire.svg",],
+    link: "https://github.com/ThisisNikkk/CityFood/",
   },
   {
     id: 3,
@@ -176,29 +176,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Web Developer Intern",
+    desc: "Designed and optimized cross-platform user interfaces and client-side applications with and React which increases the user retention by 10% .",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Summer Intern",
+    desc: "Training based internship where I developed and maintained User Interface with modern technologies.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Freelance Project",
+    desc: "Created a School Website using React and Firebase, developing front-end components and implementing code refactoring increases the efficiency by 20% .",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Lead Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies for college project.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
