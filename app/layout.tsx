e4bf3253280edbb2556defd3f82b248461e04354
Nikhil Siwan <developer.nikk@gmail.com>
2024-07-12 23:50:13 +0530

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nikhil's Portfolio",
+  title: "Nikhil's Developer Portfolio",
   description: "Modern & Minimal Developer Portfolio",
 };
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/nik-logo.png" sizes="any" />
+        <link rel="icon" href="/logo2.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
