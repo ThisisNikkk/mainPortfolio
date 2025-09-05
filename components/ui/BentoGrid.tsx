5564e945db8823d16ab002f18ffeb923e308b2f9
Nikhil Siwan <developer.nikk@gmail.com>
2025-09-05 11:22:56 +0530

    Update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,8 +51,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["Figma", "Canva", "Luminar"];
+  const rightLists = ["PhotoShop", "Midjourney", "Designer"];
 
   const [copied, setCopied] = useState(false);
 
