ce2bf08b79c27af499e2e3c4c50f50da4b70fa48
Nikhil Siwan <developer.nikk@gmail.com>
2025-09-05 11:33:05 +0530

    Footer Fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,6 @@ import MagicButton from "./MagicButton";
 const Footer = () => {
   return (
     <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
-
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
           Ready to take <span className="text-purple">your</span> digital
@@ -25,17 +24,24 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light sm:mb-10"  >
+        <p className="md:text-base text-sm md:font-normal font-light mb-4 md:mb-0">
           Copyright © 2025 Narender Kumar
         </p>
 
-        <div className="flex items-center md:gap-6 gap-6 md: mt-8">
+        <div className="flex items-center md:gap-6 gap-6 mt-2 md:mt-0">
           {socialMedia.map((info) => (
             <div
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <a href={info.link}><img src={info.img} alt="icons" width={20} height={20} /></a>
+              <a href={info.link}>
+                <img
+                  src={info.img}
+                  alt="icons"
+                  width={20}
+                  height={20}
+                />
+              </a>
             </div>
           ))}
         </div>
